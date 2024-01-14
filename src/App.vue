577460a68ae8cfc9ccf1676afe13c2ff6af59a54
YurGoV <yurgov@gmail.com>
2024-01-14 12:09:15 +0200

    ADD: navigation params. FIX: state on current url when page reloaded

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,4 @@
   <router-view />
 </template>
 
-<script setup>
-import { onMounted } from "vue";
-import { useUserStore } from "@/store/user";
-import { useRecordsStore } from "@/store/records";
-import { useContextsStore } from "@/store/contexts";
-import { loginUserByToken } from "./services/auth.service";
-
-const userStore = useUserStore();
-const recordsStore = useRecordsStore();
-const contextsStore = useContextsStore();
-
-onMounted(async () => {
-  const token = localStorage.getItem("token");
-
-  console.log(token, "in app localstorage token");
-
-  if (token) {
-    try {
-      const { user, gettedContexts, gettedRecords } =
-        await loginUserByToken(token);
-      userStore.setUser(user);
-      contextsStore.setupContexts(gettedContexts);
-      recordsStore.setupRecords(gettedRecords);
-    } catch (error) {
-      // TODO: change to error component
-      console.error("Error fetching user data:", error);
-    }
-  }
-});
-//
-</script>
+<script setup></script>
