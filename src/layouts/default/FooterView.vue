14a89c96c4da697e0c370786744dfd69cf06e98c
YurGoV <yurgov@gmail.com>
2024-03-06 22:52:51 +0200

    FIX: pomodoro count when pased

---
+++ src/layouts/default/FooterView.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- <v-footer class="bg-grey-lighten-1"> -->
+  <v-footer border>
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center mt-4" cols="12">
+        <p>{{ $t("footer.text") }}</p>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
